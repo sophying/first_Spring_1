ee118068c59e4223b08d241441dd1a12ef2b58d5
sophy <2njoybliss@gmail.com>
2019-12-16 16:58:48 +0900

    Tv Interface _ Design Pattern study

--- first_spring/src/main/java/practice/TvUser.java
+++ first_spring/src/main/java/practice/TvUser.java
@@ -4,6 +4,18 @@ public class TvUser {
 	
 	public static void main(String[] args) {
 		
+		/* Design Pattern _Factory */
+		
+		/*  
+		  객체를 생성하기 위한 인터페이스를 정의 함
+		  어떤 클래스의 인스턴스를 생성할까??  -  서브클래스에서 결정
+		  
+		  Factory Class 는 서브 역할을 하게 됨 
+		  
+		  : getBean() 으로 들어가는 값으로 어떤 인터페이스를 생성할지 결정함
+		  
+		 */
+	
 		BeanFactory factory = new BeanFactory();
 		
 		Tv tv = (Tv) factory.getBean(args[0]);
